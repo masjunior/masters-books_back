af1034351c48f7b2070b6d65957080a58087ac8d
Marco Aurelio de Sa Junior <marco.viviane.guilherme@gmail.com>
2023-11-03 15:16:45 -0300

    ajuste cliente controller

--- src/main/java/com/example/masters_of_books/cliente/ClienteController.java
+++ src/main/java/com/example/masters_of_books/cliente/ClienteController.java
@@ -1,13 +1,18 @@
 package com.example.masters_of_books.cliente;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-
+@Slf4j
 @Controller
 @RequestMapping("/clientes")
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
@@ -17,45 +22,46 @@ public class ClienteController {
     private ClienteService clienteService;
 
     @GetMapping("")
-//    public ModelAndView listarClientes() {
-//        List<Cliente> clientes = clienteService.listarClientes();
-//        ModelAndView modelAndView = new ModelAndView("listaClientes");
-//        modelAndView.addObject("clientes", clientes);
-//        return modelAndView;
-//    }
     public ResponseEntity<List<Cliente>> listarClientes() {
+        log.info("URL /clientes requested");
         List<Cliente> clientes = clienteService.listarClientes();
-        Cliente cliente = new Cliente();
-        cliente.setEmail("TESTE@123");
-        cliente.setSenha("seila");
-        cliente.setId(1L);
-        System.out.println("URL /clientes requested");
-        clientes.add(cliente);
         return ResponseEntity.ok(clientes);
     }
 
     @GetMapping("/novo")
     public String exibirFormularioNovoCliente(Model model) {
+        log.info("URL /clientes/novo requested");
         Cliente cliente = new Cliente();
         model.addAttribute("cliente", cliente);
         return "formularioCliente";
     }
 
-    @PostMapping("/salvar")
-    public String salvarCliente(@ModelAttribute("cliente") Cliente cliente) {
-        clienteService.salvarCliente(cliente);
-        return "redirect:/clientes/";
+    @GetMapping("/{id}")
+    public ResponseEntity <Cliente> consultaClientePorId(@PathVariable Long id) {
+        log.info("URL /clientes/{id} requested");
+        Cliente cliente = clienteService.buscarClientePorId(id);
+        if(cliente == null){
+            log.info("URL /clientes/{id} requested and not found");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.ok(cliente);
     }
 
-    @GetMapping("/editar/{id}")
-    public String exibirFormularioEditarCliente(@PathVariable Long id, Model model) {
+    @GetMapping("/editar/{id}") // TODO: Mudar para post.
+    public ResponseEntity <Cliente> exibirFormularioEditarCliente(@PathVariable Long id) {
+        log.info("URL /clientes/editar/{id} requested");
         Cliente cliente = clienteService.buscarClientePorId(id);
-        model.addAttribute("cliente", cliente);
-        return "formularioCliente";
+        if(cliente == null){
+            log.info("URL /clientes/editar/{id} requested and not found");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+//        model.addAttribute("cliente", cliente);
+        return ResponseEntity.ok(cliente);
     }
 
     @GetMapping("/excluir/{id}")
     public String excluirCliente(@PathVariable Long id) {
+        log.info("URL /clientes/excluir/{id} requested");
         clienteService.excluirCliente(id);
         return "redirect:/clientes/";
     }
