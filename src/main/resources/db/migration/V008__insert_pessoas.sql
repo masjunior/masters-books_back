2dbfc90f76b6ec8078609557cd36735a84246e22
Marco Aurelio de Sa Junior <marco.viviane.guilherme@gmail.com>
2023-11-03 15:16:03 -0300

    insert pessoas e clientes

---
+++ src/main/resources/db/migration/V008__insert_pessoas.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.pessoa(
+	data_nascimento, genero_id,  cpf, nome)
+	VALUES ('1982-02-17', 1,  '31842452843', 'MARCO AURELIO DE SA JUNIOR');
+INSERT INTO public.pessoa(
+	data_nascimento, genero_id,  cpf, nome)
+	VALUES ('1983-05-07', 2,  '33470025800', 'VIVIANE APARECIDA DE SA');
+COMMIT;
---
+++ src/main/resources/db/migration/V009__insert_clientes.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.cliente(
+	pessoa_id, email, senha)
+	VALUES (1, 'teste1@teste.com', '123');
+
+INSERT INTO public.cliente(
+	pessoa_id, email, senha)
+	VALUES (2, 'teste2@teste.com', '123');
+COMMIT;
